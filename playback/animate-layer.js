1b9122ee66c3a3277fe25ac4ad9fd758998fcfb4
Sharlene <17sharl@gmail.com>
2015-07-30 14:56:57 +0800

    Merge with minimap

--- playback/animate-layer.js
+++ playback/animate-layer.js
@@ -1,3 +1,4 @@
+
 $(function() {
     // Get start/end times
     var startTime = new Date(shipCoords[0].properties.time[0]);
@@ -12,7 +13,7 @@ $(function() {
       "height": "120px",
       "style": "box",
       "axisOnTop": true,
-      "showCustomTime":true
+      "showCustomTime":true,
     };
     
     // Setup timeline
@@ -2351,7 +2352,6 @@ $(function() {
         timeline.setCustomTime(new Date(ms));
     };
     
-    // 
     function onCustomTimeChange(properties) {
         if (!playback.isPlaying()) {
             playback.setCursor(properties.time.getTime());
@@ -2361,5 +2361,29 @@ $(function() {
     $("#clearBtn").click(function(){
        playback.clearData();
     }); 
+
+
+    // =====================================================
+    // =============== Minimap =============================
+    // =====================================================
+
+    var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib='Map data &copy; OpenStreetMap contributors';
+    var osm = new L.TileLayer(osmUrl, {minZoom: 5, maxZoom: 18, attribution: osmAttrib});
+
+    map.addLayer(osm);
+    // map.setView(new L.LatLng(59.92448055859924, 10.758276373601069),10);
+    
+    //Plugin magic goes here! Note that you cannot use the same layer object again, as that will confuse the two map controls
+    var osm2 = new L.TileLayer(osmUrl, {minZoom: 0, maxZoom: 13, attribution: osmAttrib });
+    var miniMap = new L.Control.MiniMap(osm2, { toggleDisplay: true }).addTo(map);
+
+
+    // =====================================================
+    // =============== Weather =============================
+    // =====================================================
+    
+    L.control.weather().addTo(map); 
+
 
 });
